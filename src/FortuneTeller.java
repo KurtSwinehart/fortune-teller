6caeddd9b57c3448df1df115d5367b20246fed61
KurtSwinehart <kurtswinehart@gmail.com>
2018-08-26 14:54:15 -0400

    Part 1 completed

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+		
+		System.out.println("What is your last name?");
+		String lastName = input.nextLine();
+		
+		System.out.println("What is your age?");
+		int age = input.nextInt();
+		
+		System.out.println("What month where you born in, using numerals 1-12?");
+		int birthMonth = input.nextInt();
+		
+		input.nextLine();
+		
+		System.out.println("What is your favorite ROYGBIV color? (Please enter HELP to get a list of ROYGBIV colors.");
+		String favoriteColor = input.nextLine();
+		if(favoriteColor.toLowerCase().equals("help")) {
+			System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, and violet.");
+			System.out.println("What is your favorite ROYGBIV color?");
+			favoriteColor = input.nextLine();
+			}
+		
+		System.out.println("How many siblings do you have?");
+		int numberSiblings = input.nextInt();
 		
 	}
 
